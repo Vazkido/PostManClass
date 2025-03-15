ad3ce7c0116c2d5f35eacc7db0ffde50a8fba6af
vazkido <vazkid02@gmail.com>
2025-03-14 20:58:30 -0400

    updated userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,6 @@
 import USer from "../models/userModel";
 import { sendMail } from "../utils/emailFunction";
 
-
 export const signin = (req, res) => {
   const { email, password } = req.body;
 };
@@ -14,9 +13,24 @@ export const signup = async (req, res) => {
       return res.status(400).send("This email is already linked to an account");
     }
     await User.create({ email, password, name });
-    sendMail()
+    sendMail();
     return res.status(200).send("User Created Successfully");
   } catch (error) {
     res.status(500).send(Error.message);
+  }
+};
+export const verifyEmail = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  try {
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    console.log(user);
+
+    user.emailVerified = true;
+    await user.save();
+    res.send("Email verified successfully");
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 };
