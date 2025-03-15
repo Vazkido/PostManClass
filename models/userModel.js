1eca935183e369638efb85afb0009ac4cbf3e395
vazkido <vazkid02@gmail.com>
2025-03-14 20:32:13 -0400

    Adding userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,14 +1,24 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name:{
-        required: true,
-        type: String
-    },
-    email:{
-        required: true,
-        unique: true,
-        type: String,
-    },
-    password
-})
+  name: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    unique: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+  emailVerified: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = mongoose.model("users", userSchema);
+export default User;
